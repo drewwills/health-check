e5688b3623098b3e1888df4db08714a52c36af51
Drew Wills <wills.drew@gmail.com>
2016-05-09 21:06:07 -0700

    New project as generated by Spring Initializer (https://start.spring.io/)

---
+++ src/main/java/org/apereo/portlet/HealthCheckApplication.java
@@ -0,0 +1,12 @@
+package org.apereo.portlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCheckApplication.class, args);
+	}
+}
