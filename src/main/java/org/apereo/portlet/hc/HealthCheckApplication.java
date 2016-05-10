afd6d19f6b4c4d99a8d85c77626480e404f1d86e
Drew Wills <wills.drew@gmail.com>
2016-05-10 07:01:57 -0700

    Convert the assembly produced by this project from a .jar to a tomcat-deployable (yet nevertheless directly runnable) .war file using steps here:  http://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#howto-create-a-deployable-war-file;  Add mavenLocal()

--- src/main/java/org/apereo/portlet/hc/HealthCheckApplication.java
+++ src/main/java/org/apereo/portlet/hc/HealthCheckApplication.java
@@ -2,11 +2,19 @@ package org.apereo.portlet.hc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HealthCheckApplication {
+public class HealthCheckApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(HealthCheckApplication.class, args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HealthCheckApplication.class);
+    }
+
 }
